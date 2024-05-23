920ab2b684c0ec07f40005e7f66940d8933ab15b
TajoDag <taodx.dev@gmail.com>
2024-05-23 10:10:44 +0700

    dqdef

--- vite.config.js
+++ vite.config.js
@@ -18,4 +18,16 @@ export default defineConfig({
       { find: "@scss", replacement: "/src/scss" },
     ],
   },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 });
