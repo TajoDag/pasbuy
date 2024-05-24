cb8412f3e9d8652deb82464ddf55b4ef7a507775
duchh <hhuuduc97@gmail.com>
2024-05-24 17:44:52 +0700

    up

--- src/pages/User/components/ListTabs/Wallet.jsx
+++ src/pages/User/components/ListTabs/Wallet.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { PiCurrencyDollar } from "react-icons/pi";
-import { AiTwotonePlusCircle } from "react-icons/ai";
+import { HiPlus } from "react-icons/hi2";
+import TranslateTing from "../../../../components/Common/TranslateTing";
 
 export const Wallet = () => {
   return (
@@ -15,14 +16,21 @@ export const Wallet = () => {
           <p style={{ fontSize: "16px", opacity: "0.8" }}>Wallet Balance</p>
         </div>
         <div className="card_item">
-          <p style={{ fontSize: "80px", border: "none" }}>
-            <AiTwotonePlusCircle />
+          <p style={{ fontSize: "70px" }}>
+            <HiPlus />
           </p>
           <p style={{ color: "red", fontSize: "18px" }}>
-            Offline Recharge Wallet
+            <TranslateTing text="Offline Recharge Wallet" />
           </p>
         </div>
-        <div className="bg_2 card_item"></div>
+        <div className="bg_2 card_item">
+          <p style={{ fontSize: "70px" }}>
+            <HiPlus />
+          </p>
+          <p style={{ fontSize: "18px" }}>
+            <TranslateTing text="Send Withdraw Request" />
+          </p>
+        </div>
       </div>
       <div className="background_white"></div>
     </div>
