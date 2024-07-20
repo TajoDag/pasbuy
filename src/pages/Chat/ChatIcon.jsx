d0ae33fd41ced7fe6a312b2fc3f8b9c8da65dd24
TajoDag <taodx.dev@gmail.com>
2024-07-20 15:47:16 +0700

    chat

--- src/pages/Chat/ChatIcon.jsx
+++ src/pages/Chat/ChatIcon.jsx
@@ -4,6 +4,108 @@ import Chat from "./Chat";
 import { ChatContext } from "../../context/ChatContext";
 
 const ChatIcon = () => {
+  // const {
+  //   userChats,
+  //   updateCurrentChat,
+  //   isChatOpen,
+  //   setIsChatOpen,
+  //   createChat,
+  // } = useContext(ChatContext);
+  // const chatIconRef = useRef(null);
+  // const [position, setPosition] = useState({
+  //   top: "auto",
+  //   bottom: "20px",
+  //   left: "auto",
+  //   right: "20px",
+  // });
+
+  // useEffect(() => {
+  //   const chatIcon = chatIconRef.current;
+  //   if (chatIcon) {
+  //     chatIcon.addEventListener("mousedown", onMouseDown);
+  //     chatIcon.addEventListener("touchstart", onTouchStart);
+  //   }
+  //   return () => {
+  //     if (chatIcon) {
+  //       chatIcon.removeEventListener("mousedown", onMouseDown);
+  //       chatIcon.removeEventListener("touchstart", onTouchStart);
+  //     }
+  //   };
+  // }, []);
+
+  // const toggleChat = async () => {
+  //   if (userChats && userChats.length < 1) {
+  //     await createChat();
+  //     setIsChatOpen((prev) => !prev);
+  //   } else {
+  //     await updateCurrentChat(userChats[0]);
+  //     setIsChatOpen((prev) => !prev);
+  //   }
+  // };
+
+  // const onMouseDown = (e) => {
+  //   e.preventDefault();
+  //   document.addEventListener("mousemove", onMouseMove);
+  //   document.addEventListener("mouseup", onMouseUp);
+  // };
+
+  // const onMouseMove = (e) => {
+  //   const { clientX, clientY } = e;
+  //   const { innerWidth, innerHeight } = window;
+  //   const iconSize = 60; // Assuming icon size is 60px
+
+  //   let newLeft = clientX - iconSize / 2;
+  //   let newTop = clientY - iconSize / 2;
+
+  //   if (newLeft < 0) newLeft = 0;
+  //   if (newTop < 0) newTop = 0;
+  //   if (newLeft + iconSize > innerWidth) newLeft = innerWidth - iconSize;
+  //   if (newTop + iconSize > innerHeight) newTop = innerHeight - iconSize;
+
+  //   setPosition({
+  //     top: newTop + "px",
+  //     bottom: "auto",
+  //     left: newLeft + "px",
+  //     right: "auto",
+  //   });
+  // };
+
+  // const onMouseUp = () => {
+  //   document.removeEventListener("mousemove", onMouseMove);
+  //   document.removeEventListener("mouseup", onMouseUp);
+  // };
+
+  // const onTouchStart = (e) => {
+  //   e.preventDefault();
+  //   document.addEventListener("touchmove", onTouchMove);
+  //   document.addEventListener("touchend", onTouchEnd);
+  // };
+
+  // const onTouchMove = (e) => {
+  //   const { clientX, clientY } = e.touches[0];
+  //   const { innerWidth, innerHeight } = window;
+  //   const iconSize = 60; // Assuming icon size is 60px
+
+  //   let newLeft = clientX - iconSize / 2;
+  //   let newTop = clientY - iconSize / 2;
+
+  //   if (newLeft < 0) newLeft = 0;
+  //   if (newTop < 0) newTop = 0;
+  //   if (newLeft + iconSize > innerWidth) newLeft = innerWidth - iconSize;
+  //   if (newTop + iconSize > innerHeight) newTop = innerHeight - iconSize;
+
+  //   setPosition({
+  //     top: newTop + "px",
+  //     bottom: "auto",
+  //     left: newLeft + "px",
+  //     right: "auto",
+  //   });
+  // };
+
+  // const onTouchEnd = () => {
+  //   document.removeEventListener("touchmove", onTouchMove);
+  //   document.removeEventListener("touchend", onTouchEnd);
+  // };
   const {
     userChats,
     updateCurrentChat,
@@ -11,7 +113,7 @@ const ChatIcon = () => {
     setIsChatOpen,
     createChat,
   } = useContext(ChatContext);
-  const chatIconRef = useRef(null);
+  const chatIconRef = useRef<HTMLDivElement>(null);
   const [position, setPosition] = useState({
     top: "auto",
     bottom: "20px",
@@ -43,13 +145,13 @@ const ChatIcon = () => {
     }
   };
 
-  const onMouseDown = (e) => {
+  const onMouseDown = (e: MouseEvent) => {
     e.preventDefault();
     document.addEventListener("mousemove", onMouseMove);
     document.addEventListener("mouseup", onMouseUp);
   };
 
-  const onMouseMove = (e) => {
+  const onMouseMove = (e: MouseEvent) => {
     const { clientX, clientY } = e;
     const { innerWidth, innerHeight } = window;
     const iconSize = 60; // Assuming icon size is 60px
@@ -106,76 +208,7 @@ const ChatIcon = () => {
     document.removeEventListener("touchmove", onTouchMove);
     document.removeEventListener("touchend", onTouchEnd);
   };
-  // const chatIconRef = useRef(null);
-  // const [position, setPosition] = useState({
-  //   top: "auto",
-  //   bottom: "20px",
-  //   left: "auto",
-  //   right: "20px",
-  // });
 
-  // useEffect(() => {
-  //   const chatIcon = chatIconRef.current;
-  //   if (chatIcon) {
-  //     chatIcon.addEventListener("mousedown", onMouseDown);
-  //     chatIcon.addEventListener("touchstart", onTouchStart);
-  //   }
-  //   return () => {
-  //     if (chatIcon) {
-  //       chatIcon.removeEventListener("mousedown", onMouseDown);
-  //       chatIcon.removeEventListener("touchstart", onTouchStart);
-  //     }
-  //   };
-  // }, []);
-
-  // const toggleChat = async () => {
-  //   if (userChats && userChats.length < 1) {
-  //     await createChat();
-  //     setIsChatOpen((prev) => !prev);
-  //   } else {
-  //     await updateCurrentChat(userChats[0]);
-  //     setIsChatOpen((prev) => !prev);
-  //   }
-  // };
-  // const onMouseDown = (e) => {
-  //   e.preventDefault();
-  //   document.addEventListener("mousemove", onMouseMove);
-  //   document.addEventListener("mouseup", onMouseUp);
-  // };
-
-  // const onMouseMove = (e) => {
-  //   setPosition({
-  //     top: e.clientY - 30 + "px",
-  //     bottom: "auto",
-  //     left: e.clientX - 30 + "px",
-  //     right: "auto",
-  //   });
-  // };
-
-  // const onMouseUp = () => {
-  //   document.removeEventListener("mousemove", onMouseMove);
-  //   document.removeEventListener("mouseup", onMouseUp);
-  // };
-
-  // const onTouchStart = (e) => {
-  //   e.preventDefault();
-  //   document.addEventListener("touchmove", onTouchMove);
-  //   document.addEventListener("touchend", onTouchEnd);
-  // };
-
-  // const onTouchMove = (e) => {
-  //   setPosition({
-  //     top: e.touches[0].clientY - 30 + "px",
-  //     bottom: "auto",
-  //     left: e.touches[0].clientX - 30 + "px",
-  //     right: "auto",
-  //   });
-  // };
-
-  // const onTouchEnd = () => {
-  //   document.removeEventListener("touchmove", onTouchMove);
-  //   document.removeEventListener("touchend", onTouchEnd);
-  // };
   return (
     <>
       <div
